059fff6e87eb0beca969806cbc943f1020968c9e
jahiddeveloper <md.jahidulislam.mar@gmail.com>
2025-12-01 09:12:42 +0600

    Card design is compleat

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import { Suspense } from "react";
 import "./App.css";
+import AvailablePlayers from "./Component/AvailablePlayers/AvailablePlayers";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
+
+let fetchPlayers = fetch("/Players.json")
+.then(res => res.json())
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
       <Hero></Hero>
+      <Suspense fallback = {<h3>Just a sec</h3>}>
+        <AvailablePlayers fetchPlayers = {fetchPlayers}></AvailablePlayers>
+      </Suspense>
     </>
   );
 }
