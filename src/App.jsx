d2b666013f941ecf416fc90a47b1a3a447138a83
jahiddeveloper <md.jahidulislam.mar@gmail.com>
2025-12-01 09:59:41 +0600

    Button toggoleing is compleat

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,51 @@
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import "./App.css";
 import AvailablePlayers from "./Component/AvailablePlayers/AvailablePlayers";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
+import SelectedPlayers from "./Component/SelectedPlayers/SelectedPlayers";
 
 let fetchPlayers = fetch("/Players.json").then((res) => res.json());
 
 function App() {
+  let [toggole, setToggole] = useState(false);
+
   return (
     <>
       <Navbar></Navbar>
       <Hero></Hero>
-      <Suspense fallback={<h3>Just a sec</h3>}>
-        <AvailablePlayers fetchPlayers={fetchPlayers}></AvailablePlayers>
-      </Suspense>
+
+      <div className="max-w-[1200px] mx-auto mt-10 flex justify-between items-center">
+        <h3 className="text-2xl font-bold">Available Players</h3>
+        <div>
+          <button
+            onClick={() => setToggole(true)}
+            className={`px-8 cursor-pointer border-r-0 py-2 border-1 border-gray-200 rounded-l-xl ${
+              toggole === true ? "bg-[#e7fe29]" : ""
+            } ${toggole === true ? "font-bold" : ""}`}
+          >
+            Available
+          </button>
+          <button
+            onClick={() => setToggole(false)}
+            className={`px-8 cursor-pointer border-l-0 py-2 border-1 border-gray-200 rounded-r-xl ${
+              toggole === false ? "bg-[#e7fe29]" : ""
+            } ${toggole === false ? "font-bold" : ""}`}
+          >
+            Selected <span>(0)</span>
+          </button>
+        </div>
+      </div>
+
+      {toggole === true ? (
+        <Suspense fallback={<h3>Just a sec</h3>}>
+          <AvailablePlayers fetchPlayers={fetchPlayers}></AvailablePlayers>
+        </Suspense>
+      ) : (
+        <Suspense fallback={<h3>Just a sec</h3>}>
+          <SelectedPlayers></SelectedPlayers>
+        </Suspense>
+      )}
     </>
   );
 }
---
+++ src/Component/SelectedPlayers/SelectedPlayers.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SelectedPlayers = () => {
+    return (
+        <div className='max-w-[1200px] mx-auto mt-8'>
+            <h1>Selected Players</h1>
+        </div>
+    );
+};
+
+export default SelectedPlayers;
