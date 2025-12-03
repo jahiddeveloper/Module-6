3058c35e030fd7485df83999daa8af9e8498896a
jahiddeveloper <md.jahidulislam.mar@gmail.com>
2025-12-03 07:28:32 +0600

    Subscribe section started

---
+++ src/Subscribe.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Subscribe = () => {
+    return (
+        <div className='font max-w-[1200px] mx-auto'>
+            <h1>Hello World</h1>
+        </div>
+    );
+};
+
+export default Subscribe;
